257704480166b13f73c9a363cd80280e05e452a7
Anthony Dalke <anthony.dalke@gmail.com>
2025-01-20 15:12:37 -0600

    Modified Files for Training
    Created .gitignore to mask cache files from commits. Modified company.py and employee.py files to continue classes training.

--- fundamentals/employee.py
+++ fundamentals/employee.py
@@ -1,8 +1,35 @@
 class Employee:
-    def __init__(self, name_first, name_last, salary):
+    def __init__(self, name_first, name_last):
         self.fname = name_first
         self.lname = name_last
+
+
+class SalaryEmployee(Employee):
+    def __init__(self, name_first, name_last, salary):
+        super().__init__(name_first, name_last)
         self.salary = salary
 
     def calculate_paycheck(self):
         return self.salary / 52
+
+
+class HourlyEmployee(Employee):
+    def __init__(self, name_first, name_last, weekly_hours, hourly_rate):
+        super().__init__(name_first, name_last)
+        self.weekly_hours = weekly_hours
+        self.hourly_rate = hourly_rate
+
+    def calculate_paycheck(self):
+        return self.weekly_hours * self.hourly_rate
+
+
+class CommissionEmployee(SalaryEmployee):
+    def __init__(self, name_first, name_last, salary, sales_count, comm_rate):
+        super().__init__(name_first, name_last, salary)
+        self.sales_count = sales_count
+        self.comm_rate = comm_rate
+
+    def calculate_paycheck(self):
+        regular_salary = super().calculate_paycheck()
+        total_commission = self.sales_count * self.comm_rate
+        return regular_salary + total_commission
