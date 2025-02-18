9ec2300a11b916821b3f501cbf3e0526664926ec
Anthony Dalke <anthony.dalke@gmail.com>
2025-02-18 12:07:24 -0600

    Created Files for Training
    Created files to follow along with demonstrations in training course.

---
+++ design_patterns/simple_factory/autos/jeepsahara.py
@@ -0,0 +1,9 @@
+from .abs_auto import AbsAuto
+
+
+class JeepSahara(AbsAuto):
+    def start(self):
+        print("Jeep Sahara started")
+
+    def stop(self):
+        print("Jeep Sahara stopped")
